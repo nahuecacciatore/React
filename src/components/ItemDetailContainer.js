aca028dab1816aab2aad9b8ba1fff8f3df09790d
nahuel cacciatore <nahuelcacciatore@gmail.com>
2022-10-04 13:13:15 -0300

    --

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -3,13 +3,7 @@ import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
 import { productos } from "./Products";
 
-const getItemDetail = () => { 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve( producto.find( p => p.id == itemId) )
-        }, 3000);
-    })
-   }
+
 
 const ItemDetailContainer = () => {
     const [producto, setProducto] = useState({})
@@ -22,7 +16,13 @@ const ItemDetailContainer = () => {
         })
     }, [])
 
-    
+    const getItemDetail = () => { 
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve( producto.find( p => p.id == itemId) )
+            }, 3000);
+        })
+       }
 
   return (
     <ItemDetail producto={producto}/>
